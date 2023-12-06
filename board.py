61585ef0662e8010fa3bc6cd9574ebf818e05448
danie1park <danielpark146@gmail.com>
2023-12-05 19:27:28 -0500

    Moved board and cell classes to sudoku_generator.py

--- board.py
+++
@@ -1,8 +0,0 @@
-class Board:
-    def __init__(self, width, height, screen, difficulty):
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.difficulty = difficulty
-
-    def draw(self):
--- sudoku_generator.py
+++ sudoku_generator.py
@@ -256,3 +256,35 @@ def generate_sudoku(size, removed):
     return board
 
 
+
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+
+    def draw(self):
+
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = pygame.display.set_mode((row, col))
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.value = value
+
+    def draw(self):
+        # Draws this cell, along with the value inside it.
+        # If this cell has a nonzero value, that value is displayed.
+        # Otherwise, no value is displayed in the cell.
+        # The cell is outlined red if it is currently selected.
+        pass
+
+    pass
